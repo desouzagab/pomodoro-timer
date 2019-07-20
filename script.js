6217b1604304d147c93eb9d2b9b719ba6d3b715c
Gabriel Souza <desouza.gabriel@icloud.com>
2019-07-20 10:20:14 -0300

    1.0

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 const app = new Vue({
     el: '#app',
-    // ========================
     data: {
         timer: null,
         totalTime: (25 * 60),
         resetButton: false,
-        title: "Let the countdown begin!!"
+        title: "Lets begin!"
     },
-    // ========================
     methods: {
         start: function() {
             this.timer = setInterval(() => this.countdown(), 1000);
             this.resetButton = true;
-            this.title = "Greatness is within sight!!"
+            this.title = "Just do it!"
         },
         stop: function() {
             clearInterval(this.timer);
             this.timer = null;
             this.resetButton = true;
-            this.title = "Never quit, keep going!!"
+            this.title = "Never quit, work your ass off!!"
         },
         reset: function() {
             this.totalTime = (25 * 60);
             clearInterval(this.timer);
             this.timer = null;
             this.resetButton = false;
-            this.title = "Let the countdown begin!!"
+            this.title = "Lets go!!"
         },
         padTime: function(time) {
             return (time < 10 ? '0' : '') + time;
@@ -39,7 +37,6 @@ const app = new Vue({
             }
         }
     },
-    // ========================
     computed: {
         min: function() {
             const min = Math.floor(this.totalTime / 60);
