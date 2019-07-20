240c5081605a046b50f646c0d294ae423b270025
Gabriel Souza <desouza.gabriel@icloud.com>
2019-07-20 08:39:37 -0300

    update script

--- script.js
+++ script.js
@@ -15,15 +15,18 @@
                  this.totaltime--
              } else {
                  this.totaltime = 0
-
+                 this.stop()
+                 alert("Over")
              }
-
          },
          reset: function() {
-
+             this.totaltime = 60 * 25
+             clearInterval(this.timer)
+             this.timer = null
          },
          stop: function() {
-
+             clearInterval(this.timer)
+             this.timer = null
          }
 
 
@@ -31,9 +34,15 @@
      },
      computed: {
          min: function() {
+             const min = Math.floor(this.totaltime / 60)
+             if (min > 10) return min
+             else return '0' + min
 
          },
          sec: function() {
+             const sec = this.totaltime - (this.min * 60)
+             if (sec > 10) return sec
+             else return '0' + sec
 
          }
      }
